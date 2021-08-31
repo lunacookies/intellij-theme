923aeac4552af709af782abdaf81990063e1af8f
Aramis Razzaghipour <aramisnoah@gmail.com>
2021-08-31 23:31:39 +1000

    Fix theme name

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,9 @@ use std::io;
 fn main() -> io::Result<()> {
     let palette = palette::Palette;
 
-    let mut intellij_theme = ThemeBuilder::new("IntelliJ".to_string(), Type::Dark);
-    imp::add_rules(&mut intellij_theme, &palette);
-    intellij_theme.build().save()?;
+    let mut builder = ThemeBuilder::new("Darcula".to_string(), Type::Dark);
+    imp::add_rules(&mut builder, &palette);
+    builder.build().save()?;
 
     Ok(())
 }
